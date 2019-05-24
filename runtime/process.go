739a07290a07060fbc7c3aa8eebae5ac2e5fc543
niusmallnan <niusmallnan@gmail.com>
2019-05-24 15:24:48 +0800

    runtime/process: do not wait forever for a process to terminate

--- runtime/process.go
+++ runtime/process.go
@@ -268,12 +268,19 @@ func (p *process) handleSigkilledShim(rst uint32, rerr error) (uint32, error) {
 			}
 
 			// wait for the process to die
+			timeout := time.After(5 * time.Second)
+			tick := time.Tick(5 * time.Millisecond)
+		loop:
 			for {
-				e := unix.Kill(p.pid, 0)
-				if e == syscall.ESRCH {
-					break
+				select {
+				case <-timeout:
+					return UnknownStatus, fmt.Errorf("containerd: giving up on %s:%s (pid %v)", p.container.id, p.id, p.pid)
+				case <-tick:
+					e := unix.Kill(p.pid, 0)
+					if e == syscall.ESRCH {
+						break loop
+					}
 				}
-				time.Sleep(5 * time.Millisecond)
 			}
 			// Create the file so we get the exit event generated once monitor kicks in
 			// without having to go through all this process again
